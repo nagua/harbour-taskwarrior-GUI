562419cea1b5ea6e7c577156c43a22b05639f748
Nicolas Riebesel <nicolas.riebesel@gmx.com>
2016-09-29 21:53:21 +0200

    Fix the watcher and get the env var properly

--- src/taskwatcher.cpp
+++ src/taskwatcher.cpp
@@ -1,8 +1,13 @@
 #include "taskwatcher.h"
+#include <QProcessEnvironment>
+#include <QDebug>
 
 TaskWatcher::TaskWatcher(QObject *parent) : QObject(parent), m_watch(new QFileSystemWatcher(this))
 {
-    m_watch->addPath("$HOME/.task/undo.data");
+    QString home = QProcessEnvironment::systemEnvironment().value("HOME");
+    QString taskfile = home + "/.task/undo.data";
+    if(!m_watch->addPath(taskfile))
+        qDebug() << "Could not watch file taskfile: " << taskfile;
     connect(m_watch, &QFileSystemWatcher::fileChanged, this, &TaskWatcher::fileHasChanged);
 }
 
